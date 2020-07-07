c5aa4b2093f3b43fda296a7fab070c5a4ad9336b
Sheena Puthanpurayil <sheena24789@gmail.com>
2020-07-07 14:05:28 +0300

    clean up

--- platform_in/app/__init__.py
+++ platform_in/app/__init__.py
@@ -11,7 +11,7 @@ from confluent_kafka.avro import AvroProducer
 import xmltodict
 import certifi
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 elastic_apm = ElasticAPM()
 # print(app.config, file=sys.stderr)
 
@@ -20,123 +20,11 @@ success_code = 202
 failure_response_object = {"status":"failure"}
 failure_code = 400
 
-counter_data_schema = """{
-  "name": "MyClass",
-  "type": "record",
-  "namespace": "fvh.peoplecounter.avro",
-  "fields": [
-    {
-      "name": "EventNotificationAlert",
-      "type": {
-        "name": "EventNotificationAlert",
-        "type": "record",
-        "fields": [
-          {
-            "name": "@version",
-            "type": "string"
-          },
-          {
-            "name": "@xmlns",
-            "type": "string"
-          },
-          {
-            "name": "ipAddress",
-            "type": "string"
-          },
-          {
-            "name": "protocolType",
-            "type": "string"
-          },
-          {
-            "name": "macAddress",
-            "type": "string"
-          },
-          {
-            "name": "channelID",
-            "type": "string"
-          },
-          {
-            "name": "dateTime",
-            "type": "int",
-            "logicalType": "date"
-          },
-          {
-            "name": "activePostCount",
-            "type": "string"
-          },
-          {
-            "name": "eventType",
-            "type": "string"
-          },
-          {
-            "name": "eventState",
-            "type": "string"
-          },
-          {
-            "name": "eventDescription",
-            "type": "string"
-          },
-          {
-            "name": "channelName",
-            "type": "string"
-          },
-          {
-            "name": "peopleCounting",
-            "type": {
-              "name": "peopleCounting",
-              "type": "record",
-              "fields": [
-                {
-                  "name": "statisticalMethods",
-                  "type": "string"
-                },
-                {
-                  "name": "TimeRange",
-                  "type": {
-                    "name": "TimeRange",
-                    "type": "record",
-                    "fields": [
-                      {
-                        "name": "startTime",
-                        "type": "int",
-                        "logicalType": "date"
-                      },
-                      {
-                        "name": "endTime",
-                        "type": "int",
-                        "logicalType": "date"
-                      }
-                    ]
-                  }
-                },
-                {
-                  "name": "enter",
-                  "type": "string"
-                },
-                {
-                  "name": "exit",
-                  "type": "string"
-                },
-                {
-                  "name": "pass",
-                  "type": "string"
-                }
-              ]
-            }
-          }
-        ]
-      }
-    }
-  ]
-}"""
-
 def delivery_report(err, msg):
     if err is not None:
         logging.error(f"Message delivery failed: {err}")
     else:
         logging.debug(f"Message delivered to {msg.topic()} [{msg.partition()}]")
-
-
 
 def kafka_avro_produce(avroProducer, topic, data):
 
@@ -165,13 +53,9 @@ def create_app(script_info=None):
     app.config.from_object(app_settings)
 
     # set up extensions
-    #elastic_apm.init_app(app)
+    elastic_apm.init_app(app)
 
-    #from app.resources.observations import observations_blueprint
-    #app.register_blueprint(observations_blueprint)
     value_schema = avro.load("avro/peoplecounterdata.avsc")
-    #value = {"name": "Value"}
-
     avroProducer = AvroProducer(
         {
             "bootstrap.servers": app.config["KAFKA_BROKERS"],
@@ -180,9 +64,9 @@ def create_app(script_info=None):
             "sasl.username": app.config["SASL_UNAME"],
             "sasl.password": app.config["SASL_PASSWORD"],
             "ssl.ca.location": certifi.where(),
-            "debug": "security,cgrp,fetch,topic,broker,protocol",
-            #"on_delivery": delivery_report,
-            "schema.registry.url": "https://kafka01.fvh.fi:8081",
+            #"debug": "security,cgrp,fetch,topic,broker,protocol",
+            "on_delivery": delivery_report,
+            "schema.registry.url": app.config["SCHEMA_REGISTRY_URL"] ,
         },
         default_value_schema=value_schema,
     )
@@ -194,25 +78,24 @@ def create_app(script_info=None):
     
     @app.route("/")
     def hello_world():
-        return jsonify(hello="world")
+        return jsonify(health="ok")
 
     @app.route('/peoplecounter/v1/', methods=['POST'])
     def post_peoplecounter_data():
         try:
-            data = request.get_data() #get_json()
-            logging.info(f"post observation: {data}")
+            data = request.get_data()
+            logging.debug(f"post observation: {data}")
             data_dict = xmltodict.parse(data, xml_attribs=False)
-            print(json.dumps(data_dict))
-
             ip = data_dict["EventNotificationAlert"]["ipAddress"]
             channel = data_dict["EventNotificationAlert"]["channelName"].replace(" ", "_")
-            topic_prefix = "test.sputhan.peoplecounter"
+            topic_prefix = "finest.peoplecounter"
             topic = f"{topic_prefix}.{ip}.{channel}"
             kafka_avro_produce(avroProducer, topic, data_dict)
             return success_response_object,success_code
 
         except Exception as e:
-            print("error", e)
+            avroProducer.flush()
+            logging.error("post data error", e)
             elastic_apm.capture_exception()
 
     return app
